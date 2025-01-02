2672509ed13b290e08b0eb3ba42e7de40864abcf
romankrutev <roman.krutev@innowise-group.com>
2025-01-02 11:16:16 +0100

    Project init

---
+++ Netflix/NetflixApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetflixApp.swift
+//  Netflix
+//
+//  Created by Raman Krutsiou on 02/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct NetflixApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
